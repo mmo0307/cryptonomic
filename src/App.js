09decaa4be13940a6cb5127691c16cd13636cb76
Misha <mmo030702@gmail.com>
2022-06-16 21:40:26 +0300

    add interval reload data

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ function App() {
 const [data, setData] = useState([]);
 const [search, setSearch] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
+    const timeInterval = setInterval(getData, 2000);
+
+    return () => {
+      clearInterval(timeInterval);
+    }
+  }, []);
+
+  const getData = () => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
       .then(res => {
         setData(res.data);
-        console.log(res.data);
       }).catch(error => console.log('Error!'));
-  }, []);
+  }
 
   const handleSearch = e => {
     setSearch(e.target.value);
